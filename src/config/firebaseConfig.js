a8cc0059d2959a99c8f294aebea5156a95aa0eeb
mrkamrann <mdkamranr25@gmail.com>
2023-12-29 14:52:52 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBdMYi9KimRd4OtPtFcVLKooKpyaXj203I",
+    authDomain: "crypto-hunter-2dcc2.firebaseapp.com",
+    projectId: "crypto-hunter-2dcc2",
+    storageBucket: "crypto-hunter-2dcc2.appspot.com",
+    messagingSenderId: "1017851281570",
+    appId: "1:1017851281570:web:9d75fdb452b6c704b317fb"
+  };
+
+  export default firebaseConfig;
